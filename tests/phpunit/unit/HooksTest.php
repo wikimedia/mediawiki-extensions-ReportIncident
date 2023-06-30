20448b35b6b9003620980d84595995268933e1b9
Kosta Harlan <kharlan@wikimedia.org>
2023-06-30 17:48:27 +0200

    Hooks: Improve test coverage and fix errors
    Why:
    
    - Higher test coverage means it's easier to refactor code, knowing that
      a test would break if something important changed
    
    What:
    
    - Add full coverage for code paths in Hooks::onBeforePageDisplay
    - Fix the existing test, which was using a nested array
    
    Bug: T337533
    Change-Id: I87a95a973346e9f6d7504bb3e277b3bbeed8417e

--- tests/phpunit/unit/HooksTest.php
+++ tests/phpunit/unit/HooksTest.php
@@ -13,27 +13,75 @@ use Title;
  */
 class HooksTest extends \MediaWikiUnitTestCase {
 
-	/**
-	 * @covers \MediaWiki\Extension\IncidentReporting\Hooks::onBeforePageDisplay
-	 */
-	public function testRlModuleIsNotLoadedIfFeatureFlagIsOff() {
+	public function testFeatureFlagDisabled() {
 		$config = new HashConfig( [
-			"IncidentReportingReportButtonEnabled" => [ "value" => true ],
-			"IncidentReportingEnabledSkins" => [ "value" => 'minerva' ],
-			"IncidentReportingEnabledNamespaces" => [ "value" => NS_USER_TALK ],
+			'IncidentReportingReportButtonEnabled' => false,
+			'IncidentReportingEnabledSkins' => [ 'minerva' ],
+			'IncidentReportingEnabledNamespaces' => [ NS_USER_TALK ],
 		] );
+		$outputPageMock = $this->createMock( OutputPage::class );
+		$outputPageMock->method( 'getConfig' )->willReturn( $config );
+		$skinMock = $this->createMock( Skin::class );
+		$outputPageMock->expects( $this->never() )->method( 'addModules' );
+		( new Hooks() )->onBeforePageDisplay( $outputPageMock, $skinMock );
+	}
+
+	public function testConfigEnabledCorrectNamespaceAndSkin() {
+		$config = new HashConfig( [
+			'IncidentReportingReportButtonEnabled' => true,
+			'IncidentReportingEnabledSkins' => [ 'minerva' ],
+			'IncidentReportingEnabledNamespaces' => [ NS_USER_TALK ],
+		] );
+		$outputPageMock = $this->createMock( OutputPage::class );
 		$title = $this->createMock( Title::class );
 		$title->expects( $this->once() )
 			->method( 'getNamespace' )
 			->willReturn( NS_USER_TALK );
-		$outputPageMock = $this->createMock( OutputPage::class );
-		$outputPageMock->method( 'getConfig' )
-			->willReturn( $config );
-		$outputPageMock->expects( $this->never() )
-			->method( 'addModules' );
+		$outputPageMock->method( 'getConfig' )->willReturn( $config );
 		$outputPageMock->method( 'getTitle' )->willReturn( $title );
 		$skinMock = $this->createMock( Skin::class );
-		( new Hooks )->onBeforePageDisplay( $outputPageMock, $skinMock );
+		$skinMock->method( 'getSkinName' )->willReturn( 'minerva' );
+		$outputPageMock->expects( $this->once() )->method( 'addModules' );
+		$outputPageMock->expects( $this->once() )->method( 'addHTML' );
+		( new Hooks() )->onBeforePageDisplay( $outputPageMock, $skinMock );
+	}
+
+	public function testConfigEnabledIncorrectNamespaceCorrectSkin() {
+		$config = new HashConfig( [
+			'IncidentReportingReportButtonEnabled' => true,
+			'IncidentReportingEnabledSkins' => [ 'minerva' ],
+			'IncidentReportingEnabledNamespaces' => [ NS_PROJECT_TALK ],
+		] );
+		$outputPageMock = $this->createMock( OutputPage::class );
+		$title = $this->createMock( Title::class );
+		$title->expects( $this->once() )
+			->method( 'getNamespace' )
+			->willReturn( NS_USER_TALK );
+		$outputPageMock->method( 'getConfig' )->willReturn( $config );
+		$outputPageMock->method( 'getTitle' )->willReturn( $title );
+		$skinMock = $this->createMock( Skin::class );
+		$skinMock->method( 'getSkinName' )->willReturn( 'minerva' );
+		$outputPageMock->expects( $this->never() )->method( 'addModules' );
+		( new Hooks() )->onBeforePageDisplay( $outputPageMock, $skinMock );
+	}
+
+	public function testConfigEnabledCorrectNamespaceIncorrectSkin() {
+		$config = new HashConfig( [
+			'IncidentReportingReportButtonEnabled' => true,
+			'IncidentReportingEnabledSkins' => [ 'minerva' ],
+			'IncidentReportingEnabledNamespaces' => [ NS_USER_TALK ],
+		] );
+		$outputPageMock = $this->createMock( OutputPage::class );
+		$title = $this->createMock( Title::class );
+		$title->expects( $this->once() )
+			->method( 'getNamespace' )
+			->willReturn( NS_USER_TALK );
+		$outputPageMock->method( 'getConfig' )->willReturn( $config );
+		$outputPageMock->method( 'getTitle' )->willReturn( $title );
+		$skinMock = $this->createMock( Skin::class );
+		$skinMock->method( 'getSkinName' )->willReturn( 'vector' );
+		$outputPageMock->expects( $this->never() )->method( 'addModules' );
+		( new Hooks() )->onBeforePageDisplay( $outputPageMock, $skinMock );
 	}
 
 }
