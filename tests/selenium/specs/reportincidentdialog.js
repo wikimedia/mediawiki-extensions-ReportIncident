f9ed263682e89ae466215a18072bdbe056e297d7
Dreamy Jazz <wbrown-ctr@wikimedia.org>
2023-10-06 22:40:52 +0100

    Add selenium tests to test the reporting dialog
    Why:
    * Tests exist for the components and on the server side, but
      there are no tests that e2e test the reporting dialog.
    * Selenium tests are the method to e2e test in MediaWiki and
      can be used to check that the reporting dialog can be opened,
      filled in and submitted
    
    What:
    * Create the necessary files in the "tests/selenium" folder,
      copying the format and common features from the MediaWiki
      page https://w.wiki/7hjB and the CheckUser extension.
    * Add the necessary packages to package.json and then update
      package-lock.json with 'npm install'.
    * Add the 'reportincident.page.js' page class and
      'reportincidentdialog.js' test spec that verify that:
    ** The dialog can be opened using the "Tools" menu
    ** The next and back buttons work
    ** The second step has a form where a report can be made
    ** Validation that required fields have content is performed
    * Update ReportIncidentDialogStep2.vue to add a class to the
      something-else textarea so that the selenium test can find
      it using the class.
    
    Bug: T346831
    Change-Id: I7c03c280125283085e59c1c8162618fe6b1a1b8f

---
+++ tests/selenium/specs/reportincidentdialog.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const assert = require( 'assert' ),
+	LoginPage = require( 'wdio-mediawiki/LoginPage.js' ),
+	ReportIncidentPage = require( '../pageobjects/reportincident.page' );
+
+describe( 'ReportIncident dialog', function () {
+	before( async () => {
+		await LoginPage.loginAdmin();
+	} );
+	beforeEach( async () => {
+		await ReportIncidentPage.open();
+	} );
+	it( 'Should open the dialog if the tools menu is used', async function () {
+		// The tools link should exist, otherwise fail.
+		assert( await ReportIncidentPage.reportLinkInToolsMenu.isExisting() );
+		// Initially the dialog should not be open
+		assert.strictEqual( await ReportIncidentPage.reportIncidentDialog.isExisting(), false );
+		// Click the report link in the tools menu
+		await ReportIncidentPage.reportLinkInToolsMenu.click();
+		// The dialog should initially be on step 1.
+		assert( await ReportIncidentPage.reportIncidentDialog.isExisting() );
+		assert( await ReportIncidentPage.stepOneContent.isExisting() );
+		assert( await ReportIncidentPage.dialogFooterNextButton.isExisting() );
+		assert( await ReportIncidentPage.dialogFooterBackButton.isExisting() );
+	} );
+	it( 'Should be able to advance to step 2 and see a form with the expected fields', async function () {
+		// Click the report link in the tools menu
+		await ReportIncidentPage.reportLinkInToolsMenu.click();
+		// Advance to step 2.
+		await ReportIncidentPage.dialogFooterNextButton.click();
+		// Step two content (which is the form) should exist, along with
+		// all the form items.
+		assert( await ReportIncidentPage.stepTwoContent.isExisting() );
+		assert( await ReportIncidentPage.harassmentOptionsFormFieldset.isExisting() );
+		assert( await ReportIncidentPage.hateSpeechOrDiscriminationOption.isExisting() );
+		assert( await ReportIncidentPage.sexualHarassmentOption.isExisting() );
+		assert( await ReportIncidentPage.threatsOrViolenceOption.isExisting() );
+		assert( await ReportIncidentPage.intimidationAggressionOption.isExisting() );
+		assert( await ReportIncidentPage.somethingElseOption.isExisting() );
+		assert( await ReportIncidentPage.additionalContentFormInput.isExisting() );
+		assert( await ReportIncidentPage.violatorFormInput.isExisting() );
+	} );
+	it( 'Should display form errors when submit attempted with no form data', async function () {
+		// Click the report link in the tools menu
+		await ReportIncidentPage.reportLinkInToolsMenu.click();
+		// Advance to step 2.
+		await ReportIncidentPage.dialogFooterNextButton.click();
+		// Go back to step 1.
+		await ReportIncidentPage.dialogFooterBackButton.click();
+		// Advance to step 2.
+		await ReportIncidentPage.dialogFooterNextButton.click();
+		// Assert the dialog is on step 2.
+		assert( await ReportIncidentPage.stepTwoContent.isExisting() );
+		// Attempt to submit the form with no data specified.
+		await ReportIncidentPage.dialogFooterNextButton.click();
+		// Check that the form displays errors on the required fields.
+		assert( await ReportIncidentPage.harassmentOptionsFieldsetFormErrors.isExisting() );
+		assert( await ReportIncidentPage.violatorFormInputErrors.isExisting() );
+		// Assert that the dialog still exists.
+		assert( await ReportIncidentPage.reportIncidentDialog.isExisting() );
+	} );
+	it( 'Should be able to submit a form with valid data', async function () {
+		// Click the report link in the tools menu
+		await ReportIncidentPage.reportLinkInToolsMenu.click();
+		// Advance to step 2.
+		await ReportIncidentPage.dialogFooterNextButton.click();
+		// Assert the dialog is on step 2.
+		assert( await ReportIncidentPage.stepTwoContent.isExisting() );
+		// Fill out the form
+		// Check some of the harassment options
+		await ReportIncidentPage.hateSpeechOrDiscriminationOption.click();
+		await ReportIncidentPage.somethingElseOption.click();
+		// Wait a bit of time for the something else textarea to appear
+		// Something else textbox should appear once something else is checked.
+		assert( await ReportIncidentPage.somethingElseTextbox.isExisting() );
+		// Add something to the something else textbox
+		await ReportIncidentPage.somethingElseTextbox.setValue( 'Testing1234' );
+		// Add a username to the violator input
+		await ReportIncidentPage.violatorFormInput.setValue( 'Admin' );
+		// Add something to the additional details box
+		await ReportIncidentPage.additionalContentFormInput.setValue( 'Additional details.' );
+		// Listen for the API request
+		await browser.setupInterceptor();
+		// Attempt to submit the form
+		await ReportIncidentPage.dialogFooterNextButton.click();
+		// Wait until the request is started before verifying the request
+		// body, url and method.
+		await browser.waitUntil(
+			async () => ( await browser.getRequests( { includePending: true } ) ).length !== 0,
+			{ timeout: 5 * 1000, timeoutMsg: 'API failed to respond in a reasonable time.' }
+		);
+		// Get the REST API request information (which will be the request at index 0
+		// as no other requests are made in the interim).
+		const request = await browser.getRequest( 0, { includePending: true } );
+		// Assert that the URL in the request goes to the REST API for submitting report data
+		assert.strictEqual( request.url, '/rest.php/reportincident/v0/report' );
+		// Assert that the request method is POST.
+		assert.strictEqual( request.method, 'POST' );
+		// Assert that the request body matches the data entered in the form.
+		assert.deepStrictEqual(
+			request.body,
+			{
+				reportedUserId: 'Admin',
+				details: 'Additional details.',
+				behaviors: [ 'hate-speech-or-discrimination', 'something-else' ],
+				somethingElseDetails: 'Testing1234',
+				revisionId: 0
+			}
+		);
+	} );
+} );
