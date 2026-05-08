b5f0dfca06056c278e1b27c1a2fe3db51a49efbd
STran <stran@wikimedia.org>
2026-05-08 07:38:17 -0700

    Migrate page definitions into Constants.harassmentTypesV2
    Why:
    As part of the migration to the V2 object, the page should be
    defined in one location instead of across the page and the Constants
    file. This next step is done in preparation to support other
    components that wil need to use this information to manage state.
    
    Change-Id: I071e7373cc4efa7f832f1b26822653fb1cc61325

--- resources/ext.reportIncident/Constants.js
+++ resources/ext.reportIncident/Constants.js
@@ -28,54 +28,399 @@ const Constants = {
 		DISRUPTIVE_EDITING: 'disruptiveediting',
 		OTHER: 'other'
 	},
+	// Pages that describe next steps for the non-emergency incident. The type
+	// should match up with what's defined in Constants.harassmentTypes.
+	// A page is described by an object with the following keys:
+	// id: the computer-friendly id; used in instrumentation, etc.
+	// labelKey: the message key for the header of the next steps page
+	// description: an object that summarizes the page; pass through the header (parsed
+	// message) and text (parsed message)
+	// notice (optional): an object that describes a notice displayed in a warning notice box;
+	// pass through the text (parsed message) and a boolean determining if the box should be
+	// displayed at all
+	// nextSteps: an array that describes messages to display in descending priority. When
+	// processed, it'll return the first valid message.
+	// helpMethods: an array that describes the possible contact methods. When processed, it'll
+	// return all valid messages.
+	// helpMethodDefault: If no helpMethods are valid, this message will be returned
 	harassmentTypesV2: {
 		DOXING: {
 			id: 'doxing',
-			labelKey: 'reportincident-dialog-harassment-type-doxing'
+			labelKey: 'reportincident-dialog-harassment-type-doxing',
+			description: {
+				header: mw.msg( 'reportincident-nonemergency-doxing-header' ),
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			notice: {
+				text: mw.msg( 'reportincident-nonemergency-doxing-notice' ),
+				shouldDisplay: () => mw.config.get( 'wgReportIncidentNonEmergencyDoxingShowWarning' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-doxing-nextsteps-configured',
+					requiredParams: [ 'wgReportIncidentNonEmergencyDoxingHideEditURL' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-doxing-nextstep-default'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-wikiemailurl',
+					requiredParams: [ 'wgReportIncidentNonEmergencyDoxingHelpMethodWikiEmailURL' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencyDoxingHelpMethodEmail' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-otherurl',
+					requiredParams: [ 'wgReportIncidentNonEmergencyDoxingHelpMethodOtherURL' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-emailstewards',
+					requiredParams: [ 'wgReportIncidentNonEmergencyDoxingHelpMethodEmailStewards' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		HATE_SPEECH: {
 			id: 'hate-or-discrimination',
-			labelKey: 'reportincident-dialog-harassment-type-hate-speech-or-discrimination'
+			labelKey: 'reportincident-dialog-harassment-type-hate-speech-or-discrimination',
+			description: {
+				header: mw.msg( 'reportincident-nonemergency-hatespeech-header' ),
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-hatespeech-nextstep'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencyHateSpeechHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencyHateSpeechHelpMethodEmail' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		INTIMIDATION: {
 			id: 'intimidation',
-			labelKey: 'reportincident-dialog-harassment-type-intimidation'
+			labelKey: 'reportincident-dialog-harassment-type-intimidation',
+			description: {
+				header: mw.msg( 'reportincident-nonemergency-intimidation-header' ),
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-intimidation-nextstep-configured',
+					requiredParams: [ 'wgReportIncidentNonEmergencyIntimidationDisputeResolutionURL' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-intimidation-nextstep-default'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencyIntimidationHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencyIntimidationHelpMethodEmail' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactcommunity',
+					requiredParams: [ 'wgReportIncidentNonEmergencyIntimidationHelpMethodContactCommunity' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		SEXUAL_HARASSMENT: {
 			id: 'sexual-harassment',
-			labelKey: 'reportincident-dialog-harassment-type-sexual-harassment'
+			labelKey: 'reportincident-dialog-harassment-type-sexual-harassment',
+			description: {
+				header: mw.msg( 'reportincident-nonemergency-sexualharassment-header' ),
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-sexualharassment-nextstep'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencySexualHarassmentHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencySexualHarassmentHelpMethodEmail' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactcommunity',
+					requiredParams: [ 'wgReportIncidentNonEmergencySexualHarassmentHelpMethodContactCommunity' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		SPAM: {
 			id: 'spam',
-			labelKey: 'reportincident-dialog-harassment-type-spam'
+			labelKey: 'reportincident-dialog-harassment-type-spam',
+			description: {
+				header: mw.msg( 'reportincident-nonemergency-spam-header' ),
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-spam-nextsteps-configured',
+					requiredParams: [ 'wgReportIncidentNonEmergencySpamSpamContentURL' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-spam-nextstep-default'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencySpamHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencySpamHelpMethodEmail' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		TROLLING: {
 			id: 'trolling',
-			labelKey: 'reportincident-dialog-harassment-type-trolling'
+			labelKey: 'reportincident-dialog-harassment-type-trolling',
+			description: {
+				header: mw.msg( 'reportincident-nonemergency-trolling-header' ),
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-trolling-nextstep'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencyTrollingHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencyTrollingHelpMethodEmail' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactcommunity',
+					requiredParams: [ 'wgReportIncidentNonEmergencyTrollingHelpMethodContactCommunity' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		SOMETHING_ELSE: {
 			id: 'something-else',
-			labelKey: 'reportincident-dialog-harassment-type-something-else'
+			labelKey: 'reportincident-dialog-harassment-type-something-else',
+			description: {
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-somethingelse-nextstep-configured',
+					requiredParams: [ 'wgReportIncidentNonEmergencyOtherDisputeResolutionURL' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-somethingelse-nextstep-default'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencySomethingElseHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencySomethingElseHelpMethodEmail' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactcommunity',
+					requiredParams: [ 'wgReportIncidentNonEmergencySomethingElseHelpMethodContactCommunity' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		SOCKPUPPETRY: {
 			id: 'sockpuppetry',
-			labelKey: 'reportincident-dialog-harassment-type-sockpuppetry'
+			labelKey: 'reportincident-dialog-harassment-type-sockpuppetry',
+			description: {
+				header: mw.msg( 'reportincident-dialog-harassment-type-sockpuppetry' ),
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-sockpuppetry-nextstep-default'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencySockpuppetryHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencySockpuppetryHelpMethodEmail' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactcommunity',
+					requiredParams: [ 'wgReportIncidentNonEmergencySockpuppetryHelpMethodContactCommunity' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		VANDALISM: {
 			id: 'vandalism',
-			labelKey: 'reportincident-dialog-harassment-type-vandalism'
+			labelKey: 'reportincident-dialog-harassment-type-vandalism',
+			description: {
+				header: mw.msg( 'reportincident-dialog-harassment-type-vandalism' ),
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-vandalism-nextstep-default'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencyVandalismHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencyVandalismHelpMethodEmail' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactcommunity',
+					requiredParams: [ 'wgReportIncidentNonEmergencyVandalismHelpMethodContactCommunity' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		USER_DISPUTE: {
 			id: 'userdispute',
-			labelKey: 'reportincident-dialog-harassment-type-userdispute'
+			labelKey: 'reportincident-dialog-harassment-type-userdispute',
+			description: {
+				header: mw.msg( 'reportincident-dialog-harassment-type-userdispute' ),
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-userdispute-nextstep-default'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencyUserDisputeHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencyUserDisputeHelpMethodEmail' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactcommunity',
+					requiredParams: [ 'wgReportIncidentNonEmergencyUserDisputeHelpMethodContactCommunity' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		DISRUPTIVE_EDITING: {
 			id: 'disruptiveediting',
-			labelKey: 'reportincident-dialog-harassment-type-disruptiveediting'
+			labelKey: 'reportincident-dialog-harassment-type-disruptiveediting',
+			description: {
+				header: mw.msg( 'reportincident-dialog-harassment-type-disruptiveediting' ),
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-disruptiveediting-nextstep-default'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencyDisruptiveEditingHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencyDisruptiveEditingHelpMethodEmail' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactcommunity',
+					requiredParams: [ 'wgReportIncidentNonEmergencyDisruptiveEditingHelpMethodContactCommunity' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		},
 		OTHER: {
 			id: 'other',
-			labelKey: 'reportincident-dialog-harassment-type-other'
+			labelKey: 'reportincident-dialog-harassment-type-other',
+			description: {
+				text: mw.msg( 'reportincident-nonemergency-generic-description' )
+			},
+			notice: {
+				text: mw.msg( 'reportincident-nonemergency-other-notice' ),
+				shouldDisplay: () => true
+			},
+			nextSteps: [
+				{
+					msgKey: 'reportincident-nonemergency-other-nextstep-default'
+				}
+			],
+			helpMethods: [
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactadmin',
+					requiredParams: [ 'wgReportIncidentNonEmergencyOtherHelpMethodContactAdmin' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-email',
+					requiredParams: [ 'wgReportIncidentNonEmergencyOtherHelpMethodEmail' ]
+				},
+				{
+					msgKey: 'reportincident-nonemergency-helpmethod-contactcommunity',
+					requiredParams: [ 'wgReportIncidentNonEmergencyOtherHelpMethodContactCommunity' ]
+				}
+			],
+			helpMethodDefault: {
+				msgKey: 'reportincident-nonemergency-helpmethod-default'
+			}
 		}
 	},
 	/**
